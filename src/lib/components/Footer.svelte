01c72c7368ccb0ea0a263ae08c67db54a7939c7c
lumap <lumap@duck.com>
2024-08-19 20:17:26 +0200

    footer: mobile rework + use separator component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+	import { Separator } from "$lib/components/ui/separator";
+
 	import { _ } from 'svelte-i18n';
 
 	const currentYear = new Date().getFullYear();
 </script>
 
-<footer class="mt-auto grid w-3/4 gap-2 pb-2">
-	<hr class="w-full border-t-2 border-gray-500" />
-	<div class="grid grid-cols-2 place-items-center gap-2">
+<footer class="mt-auto grid w-full sm:w-3/4 gap-2 pb-2">
+	<Separator />
+
+	<div class="grid sm:grid-cols-2 place-items-center gap-2">
 		<p>
 			{@html $_('footer.hosted_at', {
 				values: {
