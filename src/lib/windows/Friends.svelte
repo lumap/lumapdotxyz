aba45f671ad91fcc21d28ddf914f521b03de1ef6
lumap <lumap@duck.com>
2025-07-13 01:40:49 +0100

    add pan's btn

--- src/lib/windows/Friends.svelte
+++ src/lib/windows/Friends.svelte
@@ -19,7 +19,8 @@
             { btnURL: "https://instellate.xyz/img/button.png", website: "https://instellate.xyz" },
 			{ btnURL: "https://linkdiscord.xyz/button.png", website: "https://linkdiscord.xyz" },
 			{ btnURL: "https://pre1ude.dev/blankie.png", website: "https://pre1ude.dev" },
-			{ btnURL: "https://auxim.gay/img/webring/auxim.png", website: "https://auxim.gay" }
+			{ btnURL: "https://auxim.gay/img/webring/auxim.png", website: "https://auxim.gay" },
+			{ btnURL: "https://blog.pnly.io/content/images/2025/01/panley.png", website: "https://panley.xn--q9jyb4c/" }
         ] as { btnURL, website }}
 			{@render fren(btnURL, website)}
 		{/each}
