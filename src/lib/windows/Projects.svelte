132bafebae5fe97c6279a7d2d563cfed1bd036a3
lumap <lumap@duck.com>
2025-07-21 11:55:45 +0200

    why didnt that commit

---
+++ src/lib/windows/Projects.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { WindowTitles } from '$lib/app.svelte';
+	import Window from '$lib/components/Window.svelte';
+	const projects: string[][] = [
+	    ["This website", "https://github.com/lumap/lumapdotxyz"],
+		["PetPet (30k+ users)", 'https://github.com/lumap/petpet'],
+		["Jasper (140k+ users)", 'https://github.com/lumap/jasper'],
+		["Grow A Tree (200k+ servers)", "https://discord.com/oauth2/authorize?client_id=972637072991068220"]
+	]
+</script>
+    
+<Window title={WindowTitles.Projects}>
+	<h1 class="text-xl font-bold">Things I work on</h1>
+	<p>Sometimes in life, I decide to do stuff. Here's some of those things.</p>
+	<ul class="list-disc pl-4">
+		{#each projects as p}
+		    <li class="list-item"><a class="text-blue-500 underline" href={p[1]}>{p[0]}</a></li>
+		{/each}
+	</ul>
+</Window>
---
+++ src/lib/windows/Travel.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { WindowTitles } from '$lib/app.svelte';
+	import Window from '$lib/components/Window.svelte';
+	
+	const countries: string[] = [
+		'France',
+		'United Kingdom (only Britain)',
+		'Belgium',
+		'Spain',
+		'Portugal',
+		'Czech Republic',
+		'Switzerland',
+		'Netherlands (soon...)'
+	];
+</script>
+
+<Window title={WindowTitles.Travel}>
+	<h1 class="text-xl font-bold">Places I've been to</h1>
+	<p>
+		Here's a non-exhaustive list of countries I've visited so far. I'll keep this updated whenever I
+		visit another country! My goal is to visit as many of them as possible!
+	</p>
+	<ul class="list-disc pl-4">
+		{#each countries as c}
+		    <li class="list-item">{c}</li>
+		{/each}
+	</ul>
+</Window>
